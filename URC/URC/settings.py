01716fe57f311a8b7b66a090fcc6cedae8e438a6
Dipanshu Verma <dipanshu231099@gmail.com>
2022-02-01 12:11:47 +0530

    removed secret exposed

--- URC/URC/settings.py
+++ URC/URC/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import django_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+t%mbd=foi)ns+awgydhfp8%lpn^!1-xpzfqq99q1)@m1*_$yq'
+SECRET_KEY = django_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
